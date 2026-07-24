83b108d590f1a21aa0e7ce597b88fc062ecbe255
dalmatinec <temirbekov1602@gmail.com>
2026-07-24 22:58:59 +0500

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -5,19 +5,22 @@ def main_menu():
     """Главное меню - инлайн кнопки"""
     buttons = [
         [
-            InlineKeyboardButton(text="🌺ᴄʜᴀᴛ", callback_data="link_chat"),
-            InlineKeyboardButton(text="🌺ɴᴇᴡs", callback_data="link_news")
+            InlineKeyboardButton(text="⚜️ 𝐂𝐇𝐀𝐓", callback_data="link_chat"),
+            InlineKeyboardButton(text="⚜️ 𝐍𝐄𝐖𝐒", callback_data="link_news")
         ],
         [
-            InlineKeyboardButton(text="🌺ʀᴇsᴇʀᴠ", callback_data="link_reserve"),
-            InlineKeyboardButton(text="🌺ʙᴏᴛ", url=get_link("bot"))
+            InlineKeyboardButton(text="⚜️𝙍𝙀𝙎𝙀𝙍𝙑", callback_data="link_reserve")
         ],
         [
-            InlineKeyboardButton(text="🌺ᴍᴏʀᴇɴᴀ", url=get_link("ceo")),
-            InlineKeyboardButton(text="🌺ᴏᴘᴇʀᴀᴛᴏʀ", url=get_link("operator"))
+            InlineKeyboardButton(text="⚜️ 𝘽𝙊𝙏", url=get_link("bot")),
+            InlineKeyboardButton(text="⚜️ 𝙒𝙚𝙗𝙨𝙖𝙮𝙩", url=get_link("website"))
         ],
         [
-            InlineKeyboardButton(text="✉️ Связаться с оператором", callback_data="support")
+            InlineKeyboardButton(text="⚜️ 𝙈𝙊𝙍𝙀𝙉𝘼", url=get_link("ceo")),
+            InlineKeyboardButton(text="⚜️ 𝙊𝙋𝙀𝙍𝘼𝙏𝙊𝙍", url=get_link("operator"))
+        ],
+        [
+            InlineKeyboardButton(text="📤 Пиши если спам", callback_data="support")
         ]
     ]
     return InlineKeyboardMarkup(inline_keyboard=buttons)
