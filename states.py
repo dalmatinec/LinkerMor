33f758f2f53142d7f3268a8a2fdc4d3248c0386b
dalmatinec <temirbekov1602@gmail.com>
2026-09-19 14:26:54 +0500

    Delete states.py

--- states.py
+++
@@ -1,8 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-
-class EditLinkStates(StatesGroup):
-    choosing_link = State()
-    entering_value = State()
-
-class EditOperatorsStates(StatesGroup):
-    entering_operator = State()
